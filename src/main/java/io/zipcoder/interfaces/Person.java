55793463add9aa731099fad05262a3ab0ce8d343
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-12-26 19:24:23 -0500

    Person class+tests

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -1,5 +1,23 @@
 package io.zipcoder.interfaces;
 
 public class Person {
+    public final long id;
+    public String name;
 
+    public Person(long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
 }
--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,30 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+    public void testPersonConstructor(){
+        long id = 1L;
+        String name = "Tam";
+        Person person = new Person(id, name);
+
+        Assert.assertEquals(id, person.getId());
+        Assert.assertEquals(name, person.getName());
+    }
+
+    @Test
+    public void testPersonSetName(){
+        long id = 1L;
+        String name = "Tam";
+        Person person = new Person(id, name);
+        String newName = "Maria";
+
+        person.setName(newName);
+
+        Assert.assertEquals(newName, person.getName());
+    }
 
 }
