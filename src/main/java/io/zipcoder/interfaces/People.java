00f5fe9d655203b90696fa20601eb6e8947c91d4
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-12-29 23:20:44 -0500

    rewriting People

--- src/main/java/io/zipcoder/interfaces/People.java
+++ src/main/java/io/zipcoder/interfaces/People.java
@@ -1,9 +1,19 @@
 package io.zipcoder.interfaces;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class People extends ArrayList<Person> {
+    List<Person> personList;
+
+    public People(){
+        personList = new ArrayList<>();
+    }
+
+    public boolean add(Person p){
+        return personList.add(p);
+    }
 
     public Person findById(long id){
         for(Person p: this){
@@ -14,4 +24,32 @@ public class People extends ArrayList<Person> {
 
         return null;
     }
+
+    public boolean contains(Person p){
+        return this.personList.contains(p);
+    }
+
+    public boolean remove(Person p){
+        return this.personList.remove(p);
+    }
+
+    public boolean remove(Long id){
+        for(Person p: this.personList){
+            if(p.getId() == id){
+                return this.personList.remove(p);
+            }
+        }
+
+        return false;
+    }
+
+    public void removeAll(){
+        this.personList.removeAll(this.personList);
+    }
+
+    public int count(){
+        return this.personList.size();
+    }
+
+
 }
